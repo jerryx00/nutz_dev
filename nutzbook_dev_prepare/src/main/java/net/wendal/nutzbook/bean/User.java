093ef4f098b225b21283dc3e63141b8cd84c3616
jerryxu <xiang37202@163.com>
2018-06-19 08:20:19 +0800

    修改gitignore

--- nutzbook_dev_prepare/src/main/java/net/wendal/nutzbook/bean/User.java
+++ nutzbook_dev_prepare/src/main/java/net/wendal/nutzbook/bean/User.java
@@ -1,30 +1,76 @@
 package net.wendal.nutzbook.bean;
 
-import org.nutz.dao.entity.annotation.*;
+import java.util.List;
+
+import org.nutz.dao.entity.annotation.ColDefine;
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.ManyMany;
+import org.nutz.dao.entity.annotation.Name;
+import org.nutz.dao.entity.annotation.One;
+import org.nutz.dao.entity.annotation.Table;
 
 @Table("t_user")
 public class User extends BasePojo {
 
 	@Id
-	private int id;
+	protected int id;
 
-	public int getUid() {
-		return uid;
+	public int getId() {
+		return id;
 	}
 
-	public void setUid(int uid) {
-		this.uid = uid;
+	public void setId(int id) {
+		this.id = id;
 	}
 
-	@Column
-	private int uid;
-	@Name
-	@Column
-	private String name;
-	@Column("passwd")
-	private String password;
-	@Column
-	private String salt;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public boolean isLocked() {
+		return locked;
+	}
+
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	public List<Permission> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(List<Permission> permissions) {
+		this.permissions = permissions;
+	}
 
 	public UserProfile getProfile() {
 		return profile;
@@ -34,34 +80,20 @@ public class User extends BasePojo {
 		this.profile = profile;
 	}
 
-	//key表示对方表字段的key, 即是target中的字段
-	@One(target = UserProfile.class,field = "id",key="userId")
+	@Name
+	@Column
+	protected String name;
+	@Column("passwd")
+	@ColDefine(width=128)
+	protected String password;
+	@Column
+	protected String salt;
+	@Column
+	private boolean locked;
+	@ManyMany(from="u_id", relation="t_user_role", target=Role.class, to="role_id")
+	protected List<Role> roles;
+	@ManyMany(from="u_id", relation="t_user_permission", target=Permission.class, to="permission_id")
+	protected List<Permission> permissions;
+	@One(target=UserProfile.class, field="id", key="userId")
 	protected UserProfile profile;
-
-
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getSalt() {
-		return salt;
-	}
-	public void setSalt(String salt) {
-		this.salt = salt;
-	}
-	
 }
