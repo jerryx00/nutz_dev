8fdd9c7eb935248f610c400bcb986eda855fb991
jerryxu <xiang37202@163.com>
2018-06-20 09:14:14 +0800

    Module要加 @IocBean;Beetl正常

--- nutzbook_dev_prepare/src/main/java/net/wendal/nutzbook/module/BeetlTemplateModule.java
+++ nutzbook_dev_prepare/src/main/java/net/wendal/nutzbook/module/BeetlTemplateModule.java
@@ -1,25 +1,63 @@
 package net.wendal.nutzbook.module;
 
+import net.wendal.nutzbook.bean.User;
 import net.wendal.nutzbook.bean.UserProfile;
+import org.nutz.dao.Cnd;
 import org.nutz.dao.QueryResult;
 import org.nutz.dao.pager.Pager;
-import org.nutz.mvc.annotation.At;
-import org.nutz.mvc.annotation.Fail;
-import org.nutz.mvc.annotation.Ok;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Strings;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
+import org.nutz.mvc.annotation.*;
 
-@At("/xugp") // 整个模块的路径前缀
+@IocBean // 声明为Ioc容器中的一个Bean, 这个不能少，否则就会出现 NullPointerException 异常
 
+@At("/beetl") // 整个模块的路径前缀
 public class BeetlTemplateModule extends BaseModule{
 
-    @At
+    private static final Log log = Logs.get();
+
+    @At({"/hello/?", "/hello/"})
     @Ok("beetl:/hello.html")
     @Fail("void") // beelt的机制导致只能使用void,详细原因看nutzbook中的代码吧
     public Object hello() {
+        log.debug("BeetlTemplateModule begin hello... ");
+        QueryResult qr = new QueryResult();
+        Pager pager = dao.createPager(1, 20);
+        pager.setRecordCount(dao.count(UserProfile.class));
+        log.debug("QueryResult qr: " + qr);
+        log.debug("UserProfile count: " + dao.count(UserProfile.class));
+
+        qr.setPager(pager);
+        qr.setList(dao.query(UserProfile.class, null, pager));
+        return qr;
+    }
+
+
+    @At("/json")
+//    @Ok("beetl:/hello.html")
+    @Fail("void") // beelt的机制导致只能使用void,详细原因看nutzbook中的代码吧
+    public Object json() {
+        log.debug("BeetlTemplateModule begin json... ");
         QueryResult qr = new QueryResult();
         Pager pager = dao.createPager(1, 20);
         pager.setRecordCount(dao.count(UserProfile.class));
         qr.setPager(pager);
         qr.setList(dao.query(UserProfile.class, null, pager));
         return qr;
+    }
+
+
+    @At("/query")
+    @Ok("json:full")
+    public Object query(@Param("name")String name, @Param("..")Pager pager) {
+        log.debug("BeetlTemplateModule begin query... ");
+        Cnd cnd = Strings.isBlank(name)? null : Cnd.where("name", "like", "%"+name+"%");
+        QueryResult qr = new QueryResult();
+        qr.setList(dao.query(User.class, cnd, pager));
+        pager.setRecordCount(dao.count(User.class, cnd));
+        qr.setPager(pager);
+        return qr; //默认分页是第1页,每页20条
     }
 }
